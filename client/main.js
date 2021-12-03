f19063523837bc0d31157c83920c16c298ea6c37
corentin <corentincolnat@gmail.com>
2021-12-03 15:45:11 +0100

    App avec splash screen persoonnalisé

--- client/main.js
+++ client/main.js
@@ -1,1 +1,17 @@
 import '../imports/ui/App.js';
+
+var handle = LaunchScreen.hold();
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+  navigator.splashscreen.show();
+}
+
+setTimeout(()=> {
+  handle.release();
+}, 10000);
+
+
+
+
+
